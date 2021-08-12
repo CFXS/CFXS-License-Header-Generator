750f9338d16567dfe4dac111b601004b30abea51
RVeips <rihardsv@na.lv>
2021-08-12 16:53:19 +0300

    format

--- UpdateHeaders_GPLv3.py
+++ UpdateHeaders_GPLv3.py
@@ -1,5 +1,5 @@
 # CFXS License Header Generator
-# Copyright (C) 2021 - CFXS / Rihards Veips <https://github.com/CFXS/CFXS-License-Header-Generator>
+# Copyright (C) 2021 | CFXS / Rihards Veips <https://github.com/CFXS/CFXS-License-Header-Generator>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
